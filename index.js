9e8ebcb58ed42ef822c1696f9169602a9cb81329
Student Sahil-Aggarwal937 <sahilaggarwal937@gmail.com>
2024-06-27 09:18:15 +0530

    github data added

--- index.js
+++ index.js
@@ -5,6 +5,31 @@ const express = require('express')
 
 const app = express()
 
+const data = {
+  "_id": "667697d358fe36d65a0f2c49",
+  "uId": "4d94501d1f78ac091dbc9b4d",
+  "uNm": "Adrien Joly",
+  "text": "thank you Béton Coton for the discovery!",
+  "name": "🥬 Lettuce - The Lobbyist",
+  "eId": "/yt/xRP56JQqe3w",
+  "ctx": "bk",
+  "pl": {
+    "id": 11,
+    "name": "🕶 soul / funk / jazz / etc..."
+  },
+  "img": "https://i.ytimg.com/vi/xRP56JQqe3w/default.jpg",
+  "src": {
+    "id": "https://www.youtube.com/watch?v=xRP56JQqe3w",
+    "name": "🥬 Lettuce - The Lobbyist - YouTube"
+  },
+  "lov": [
+    "518b5a447e91c862b2adea1a",
+    "53f4ee46e04b7b4fca7dcea3"
+  ],
+  "nbP": 3,
+  "nbR": 1
+}
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -20,6 +45,10 @@ app.get('/login',(req,res) =>{
 
 app.get('/youtube',(req,res)=>{
   res.send(`<h2>Backend Practise</h2>`)
+})
+
+app.get('/github',(req,res)=>{
+  res.json(data)
 })
 
 app.listen(PORT, () => {
